d1ccef445195ae555734ddd43670aff678ad8795
Kutterhead <esseanx@gmail.com>
2025-02-04 21:48:38 +0100

    Implementada función de detección. Bolos se cuentan al caer.

--- Assets/gameController.cs
+++ Assets/gameController.cs
@@ -9,12 +9,52 @@ public class gameController : MonoBehaviour
     public GameObject bola;
     public Transform puntero;
 
+
+    public GameObject[] bolosObjects;
+
+    bool[] checkBolos = new bool[] { false, false, false, false, false, false, false, false, false, false };
+
+
+    public int caidos = 0;
+
     void Start()
     {
         // bola.GetComponent<Rigidbody>().AddForce(puntero.forward*10,ForceMode.Impulse);
         //bola.GetComponent<Rigidbody>().AddForce(puntero.forward * 10, ForceMode.VelocityChange);
+
+        caidos = 0;
+        Invoke("dispara", 1f);
+
+        bolosObjects = GameObject.FindGameObjectsWithTag("bolo");
+        //compruebaBolos();
+        Invoke("compruebaBolos", 5f);
+    }
+    public void dispara()
+    {
         bola.GetComponent<Rigidbody>().velocity = puntero.forward * 10;
 
+    }
+
+
+    public void compruebaBolos(){
+
+        Debug.Log("Comprobando bolos");
+        caidos = 0;
+        for (int i = 0; i < bolosObjects.Length;i++)
+        {
+            if (bolosObjects[i].transform.eulerAngles.x>10f || bolosObjects[i].transform.eulerAngles.z > 10f)
+            {
+                checkBolos[i] = true;//si ha caído
+                caidos++;
+            }
+            else
+            {
+                checkBolos[i] = false;//no ha caído
+            }
+            //Debug.Log("bolo caído " + i + ": " + checkBolos[i]);
+        }
+        Debug.Log("Han caído " + caidos + " bolos.");
+        Debug.Log("En pie " + (bolosObjects.Length - caidos) + " bolos.");
     }
 
     // Update is called once per frame

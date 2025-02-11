1852c19c21c70a76712cc5566525fe0f82cb117e
Kutterhead <esseanx@gmail.com>
2025-02-11 21:27:00 +0100

    Incorporado tablero. Incorporados bolos con collider capsule. implementado sistema de reinicio.

--- Assets/gameController.cs
+++ Assets/gameController.cs
@@ -17,21 +17,58 @@ public class gameController : MonoBehaviour
 
     public int caidos = 0;
 
+    
+    int puntos = 0;
+    int ronda = 0;
+
+
+    public Vector3[] posiciones;
+
     void Start()
     {
         // bola.GetComponent<Rigidbody>().AddForce(puntero.forward*10,ForceMode.Impulse);
         //bola.GetComponent<Rigidbody>().AddForce(puntero.forward * 10, ForceMode.VelocityChange);
 
         caidos = 0;
-        Invoke("dispara", 1f);
+        puntos = 0;
 
         bolosObjects = GameObject.FindGameObjectsWithTag("bolo");
         //compruebaBolos();
-        Invoke("compruebaBolos", 5f);
+        System.Array.Resize(ref posiciones, bolosObjects.Length);
+
+        for (int i =0; i < bolosObjects.Length; i++ )
+        {
+            posiciones[i] = bolosObjects[i].transform.position;
+
+        }
+
+
     }
+
+    public void resetBolos()
+    {
+        for (int i = 0; i < bolosObjects.Length; i++)
+        {
+            bolosObjects[i].transform.position = posiciones[i];
+            bolosObjects[i].transform.eulerAngles = Vector3.zero;
+
+            //bolosObjects[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+            bolosObjects[i].GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            bolosObjects[i].GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+
+        }
+        Debug.Log("Resetea bolos");
+    }
+
+
+
+
     public void dispara()
     {
-        bola.GetComponent<Rigidbody>().linearVelocity = puntero.forward * 10;
+
+        bola.transform.position = puntero.position;
+        bola.GetComponent<Rigidbody>().linearVelocity = puntero.forward * 20;
 
     }
 
@@ -46,6 +83,7 @@ public class gameController : MonoBehaviour
             {
                 checkBolos[i] = true;//si ha ca�do
                 caidos++;
+                puntos++;
             }
             else
             {
@@ -53,13 +91,35 @@ public class gameController : MonoBehaviour
             }
             //Debug.Log("bolo ca�do " + i + ": " + checkBolos[i]);
         }
-        Debug.Log("Han ca�do " + caidos + " bolos.");
+
+        if (caidos == bolosObjects.Length)
+        {
+            //premio pleno!
+            Debug.Log("Has hecho pleno!!! " + caidos + " bolos.");
+            puntos += 10;
+        }
+        else
+        {
+
+        Debug.Log("Han caído " + caidos + " bolos.");
+
         Debug.Log("En pie " + (bolosObjects.Length - caidos) + " bolos.");
+        }
+
+        Invoke("resetBolos",1f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Invoke("dispara", 1f);
+
+            dispara();
+            Invoke("compruebaBolos", 5f);
+
+        }
     }
 }
